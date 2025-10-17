6472336f00fbb7956254225fe65ba76d1b250d28
Spandana-MJ <spandanaspandumj@gmail.com>
2025-10-17 07:08:28 +0530

    fix for render deployment

--- server/server.js
+++ server/server.js
@@ -20,6 +20,7 @@
 // app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
 
+
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
@@ -37,18 +38,6 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/items", require("./routes/items"));
 app.use("/api/admin", require("./routes/admin"));
-
-// Serve React frontend
-
-if (process.env.NODE_ENV === 'production') {
-  const __dirnameRoot = path.resolve(__dirname, '..'); // go up one folder
-  app.use(express.static(path.join(__dirnameRoot, 'client', 'dist')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirnameRoot, 'client', 'dist', 'index.html'));
-  });
-}
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
